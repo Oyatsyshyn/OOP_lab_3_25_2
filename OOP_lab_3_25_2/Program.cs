4f48b8e79d29c47af3c791b587a4410717fc9e88
Роман Данилевич <dwordchanel@gmail.com>
2020-05-27 02:16:00 +0300

    Add project files.

---
+++ OOP_lab_3_25_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_lab_3_25_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader file = new StreamReader("input.txt");
+
+            string[] numbers = file.ReadToEnd().Split(new char[] { '\r', '\n', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int[] a = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; ++i)
+            {
+                a[i] = int.Parse(numbers[i]);
+            }
+
+            int max = a[0];
+
+            for (int i = 0; i < numbers.Length; ++i)
+            {
+                if (max <= a[i])
+                {
+                    max = a[i];
+                }
+            }
+
+            Console.WriteLine("Hайбiльший елемент: {0}", max);
+        }
+    }
+}
